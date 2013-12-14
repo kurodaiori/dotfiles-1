b6a0e78570c23693d7c58fb5a93270db12922e08
Shin Kojima <shin@kojima.org>
2013-12-15 06:56:56 +0900

    perl mod template

--- dzil/profiles/default/Module.pm
+++ dzil/profiles/default/Module.pm
@@ -3,6 +3,22 @@ use warnings;
 package {{$name}};
 # ABSTRACT: TODO: abstract here
 
+sub new {
+    my ($class, %args) = @_;
+    my $self = bless {
+        %args,
+    }, $class;
+    return $self->init;
+}
+
+sub init {
+    my $self = shift;
+
+    # TODO: initialize
+
+    return $self;
+}
+
 1;
 __END__
 
